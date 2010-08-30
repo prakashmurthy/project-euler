66f02b09c573a12f62a08db4ae460983c8ff7ef7
Prakash Murthy <prakblr@yahoo.com>
2010-08-30 03:23:09 -0600

    Modified to use the factorial function from lib directory

--- projecteuler/prob_020_sum_of_100factorial.rb
+++ projecteuler/prob_020_sum_of_100factorial.rb
@@ -1,16 +1,7 @@
 #!/usr/bin/env ruby
 # Solution for Problem number 20 in Project Euler 
 # http://projecteuler.net/index.php?section=problems&id=20
-
-class Integer
-  def factorial 
-    if self <= 1
-      1
-    else 
-      self * ( self -1 ).factorial
-    end
-  end
-end
+require "../lib/factorial"
 
 hundred_factorial = 100.factorial
 total = 0
--- projecteuler/prob_034_sum_of_factorials.rb
+++ projecteuler/prob_034_sum_of_factorials.rb
@@ -4,16 +4,7 @@
 
 # Assuming an upper bound of 1000000 (don't know the logic of the upper bound)
 
-class Integer
-  def factorial 
-    if self <= 1
-      1
-    else 
-      self * ( self -1 ).factorial
-    end
-  end
-end
-
+require "../lib/factorial"
 
 (10..1000000).each do |num|
   total = 0
