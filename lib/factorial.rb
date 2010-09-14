04928fff1c3954796e98cfc368459b59306a0c4c
Prakash Murthy <prakblr@yahoo.com>
2010-09-14 13:58:22 -0600

    Added factorials_sum function to lib/factorial.rb

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -9,4 +9,16 @@ class Integer
       self * ( self -1 ).factorial
     end
   end
+  
+  def factorials_sum
+    self_str = self.to_s
+    length = self_str.length
+    factorials_sum = 0
+    for count in 1..length
+      #puts "#{self_str[(count-1), 1]}"
+      digit = self_str[(count-1), 1].to_i
+      factorials_sum += digit.factorial
+    end
+    factorials_sum
+  end
 end
