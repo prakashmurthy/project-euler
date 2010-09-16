12de3939f3ed1404e9e62c9c89b3348c05f1dd8a
Prakash Murthy <prakblr@yahoo.com>
2010-09-16 13:10:33 -0600

    Refactoring: Moved two functions to common file lib/integer_functions.rb

--- projecteuler/prob_021_amicable_numbers.rb
+++ projecteuler/prob_021_amicable_numbers.rb
@@ -1,33 +1,7 @@
 #!/usr/bin/env ruby
 # Solution to problem number 21 on Project Euler
 # http://projecteuler.net/index.php?section=problems&id=21
-
-def proper_divisors(number)
-  i = 1
-  divisors_array = []
-  
-  while i < number
-    #puts "Came here with i = #{i}"
-    if number.modulo(i) == 0
-      divisors_array.push(i)
-    end
-    
-    if ((i >= (number/2)) and (i < number))
-      i = number
-    else
-      i = i + 1
-    end
-  
-  end
-  divisors_array
-end
-
-def sum_of_proper_divisors(number)
-  divisors = proper_divisors(number)
-  total = 0
-  divisors.each {|num| total = total + num}
-  total
-end 
+require "../lib/integer_functions"
 
 # puts proper_divisors(6)
 # puts sum_of_proper_divisors(6)
@@ -36,9 +10,9 @@ end
 
 amicable_numbers_array = []
 (2..10000).each do |num|
-  sum = sum_of_proper_divisors(num)
+  sum = num.sum_of_proper_divisors
   if sum != num
-    new_num = sum_of_proper_divisors(sum)
+    new_num = sum.sum_of_proper_divisors
     amicable_numbers_array.push(num) if num == new_num  
     puts "Now processing #{num}" if num.modulo(400) == 0
   end
