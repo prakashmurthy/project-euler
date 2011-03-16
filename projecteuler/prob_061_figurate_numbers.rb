970e05952aa25f9a2efc4fc06514cdf0c01e1a59
prakashmurthy <prakblr@yahoo.com>
2011-03-16 00:37:29 -0600

    Wrote some ugly code in nearly 3 hours to solve problem 61. More importantly though, I SOLVED THE PROBLEMgit add projecteuler/prob_061_figurate_numbers.rb

--- projecteuler/prob_061_figurate_numbers.rb
+++ projecteuler/prob_061_figurate_numbers.rb
@@ -7,12 +7,24 @@ puts "Starting up 3... 2... 1... GOOOOOOOOOO!\n\n"
 
 MIN_NUMBER = 1_000
 MAX_NUMBER = 10_000
-tri_numbers = []
-sqr_numbers = []
-penta_numbers = []
-hexa_numbers = []
-hepta_numbers = []
-octa_numbers = []
+@tri_numbers = []
+@sqr_numbers = []
+@penta_numbers = []
+@hexa_numbers = []
+@hepta_numbers = []
+@octa_numbers = []
+
+class Integer
+  def third_number_non_zero?
+    self.to_s[2] != '0'
+  end
+end
+
+def next_elements(number, list_of_numbers)
+  last_two_digits = number.to_s[2,2]
+  next_elements_list = list_of_numbers.select {|second_number| second_number.to_s =~ /^#{last_two_digits.to_i}/}
+  return next_elements_list
+end
 
 n = 1
 while true 
@@ -24,53 +36,124 @@ while true
   octa_number = n * (3 * n -2 )
   
   break if tri_number > MAX_NUMBER
-  tri_numbers << tri_number if tri_number < MAX_NUMBER and tri_number >= MIN_NUMBER
-  sqr_numbers << sqr_number if sqr_number < MAX_NUMBER and sqr_number >= MIN_NUMBER
-  penta_numbers << penta_number if penta_number < MAX_NUMBER and penta_number >= MIN_NUMBER
-  hexa_numbers << hexa_number if hexa_number < MAX_NUMBER and hexa_number >= MIN_NUMBER
-  hepta_numbers << hepta_number if hepta_number < MAX_NUMBER and hepta_number >= MIN_NUMBER
-  octa_numbers << octa_number if octa_number < MAX_NUMBER and octa_number >= MIN_NUMBER
+  @tri_numbers << tri_number if ( (tri_number < MAX_NUMBER) and (tri_number >= MIN_NUMBER) and (tri_number.third_number_non_zero?) )
+  @sqr_numbers << sqr_number if ( (sqr_number < MAX_NUMBER) and (sqr_number >= MIN_NUMBER) and (sqr_number.third_number_non_zero?) )
+  @penta_numbers << penta_number if ( (penta_number < MAX_NUMBER) and (penta_number >= MIN_NUMBER) and (penta_number.third_number_non_zero?) )
+  @hexa_numbers << hexa_number if ( (hexa_number < MAX_NUMBER) and (hexa_number >= MIN_NUMBER) and (hexa_number.third_number_non_zero?) )
+  @hepta_numbers << hepta_number if ( (hepta_number < MAX_NUMBER) and (hepta_number >= MIN_NUMBER) and (hepta_number.third_number_non_zero?) )
+  @octa_numbers << octa_number if ( (octa_number < MAX_NUMBER) and (octa_number >= MIN_NUMBER) and (octa_number.third_number_non_zero?) )
   n += 1
 end
 
-puts "tri_numbers are #{tri_numbers.join(', ')}\n\n"
-puts "sqr_numbers are #{sqr_numbers.join(', ')}\n\n"
-puts "penta_numbers are #{penta_numbers.join(', ')}\n\n"
-# puts "hexa_numbers are #{hexa_numbers.join(', ')}"
-# puts "hepta_numbers are #{hepta_numbers.join(', ')}"
-# puts "octa_numbers are #{octa_numbers.join(', ')}"
+puts "There are #{@tri_numbers.length} tri_numbers are #{@tri_numbers.join(', ')}\n\n"
+puts "There are #{@sqr_numbers.length} sqr_numbers are #{@sqr_numbers.join(', ')}\n\n"
+puts "There are #{@penta_numbers.length} penta_numbers are #{@penta_numbers.join(', ')}\n\n"
+puts "There are #{@hexa_numbers.length} hexa_numbers are #{@hexa_numbers.join(', ')}\n\n"
+puts "There are #{@hepta_numbers.length} hepta_numbers are #{@hepta_numbers.join(', ')}\n\n"
+puts "There are #{@octa_numbers.length} octa_numbers are #{@octa_numbers.join(', ')}\n\n"
 
-def next_element_array(number, number_array)
-  number_array.select { |num2| num2.to_s =~ /^#{number.to_s[2,2].to_i}/ }
-end
-
-# puts "next elements for 2010 in penta_numbers is #{next_element_array(2010, penta_numbers).join(', ')}"
-
-remaining_arrays = [sqr_numbers, tri_numbers]
-result_array = []
-
-penta_numbers.each do |number|
-# number = penta_numbers[0]
-  result_array << number
-  count = 0
-  while !remaining_arrays.empty?
-    observing_array = remaining_arrays[count]
-    next_array = next_element_array(result_array[-1], observing_array)
-    
-    if !next_array.empty?
-      remaining_arrays.delete(observing_array)
-      
-      next_array.each_with_index do |next_number, index|
-        result_array << next_number
-        count = 0
-        
-        puts "next_number # #{index} is #{next_number}"
-      end
-    else
-      count += 1
+@octa_numbers.each do |octal|
+  remaining_numbers = @tri_numbers + @sqr_numbers + @penta_numbers + @hexa_numbers + @hepta_numbers
+  list = next_elements(octal, remaining_numbers)
+  # unless list.empty?
+  #   puts "Octal number #{octal} has the following linked elements: #{list.join(', ')}\n\n"
+  # end
+  list.each do |second_number|
+    if @hepta_numbers.include?(second_number)
+      remaining_numbers = remaining_numbers - @hepta_numbers
+    elsif @hexa_numbers.include?(second_number)
+      remaining_numbers = remaining_numbers - @hexa_numbers
+    elsif @penta_numbers.include?(second_number)
+      remaining_numbers = remaining_numbers - @penta_numbers
+    elsif @sqr_numbers.include?(second_number)
+      remaining_numbers = remaining_numbers - @sqr_numbers
+    elsif @tri_numbers.include?(second_number)
+      remaining_numbers = remaining_numbers - @tri_numbers
     end
     
+    second_level_numbers = remaining_numbers
+    second_list = next_elements(second_number, remaining_numbers)
+    
+    # unless second_list.empty?
+    #   puts "Octal number #{octal} => Second number #{second_number} has the following linked list: #{second_list.join(', ')}\n\n"
+    # end
+    
+    second_list.each do |third_number|
+      remaining_numbers = second_level_numbers
+      if @hepta_numbers.include?(third_number)
+        remaining_numbers = remaining_numbers - @hepta_numbers
+      elsif @hexa_numbers.include?(third_number)
+        remaining_numbers = remaining_numbers - @hexa_numbers
+      elsif @penta_numbers.include?(third_number)
+        remaining_numbers = remaining_numbers - @penta_numbers
+      elsif @sqr_numbers.include?(third_number)
+        remaining_numbers = remaining_numbers - @sqr_numbers
+      elsif @tri_numbers.include?(third_number)
+        remaining_numbers = remaining_numbers - @tri_numbers
+      end
+      
+      third_level_numbers = remaining_numbers
+      third_list = next_elements(third_number, remaining_numbers)
+      
+      # unless third_list.empty?
+      #   puts "#{octal} => #{second_number} => #{third_number} => #{third_list.join(', ')}\n\n"
+      # end
+      
+      third_list.each do |fourth_number|
+        remaining_numbers = third_level_numbers
+        if @hepta_numbers.include?(fourth_number)
+          remaining_numbers = remaining_numbers - @hepta_numbers
+        elsif @hexa_numbers.include?(fourth_number)
+          remaining_numbers = remaining_numbers - @hexa_numbers
+        elsif @penta_numbers.include?(fourth_number)
+          remaining_numbers = remaining_numbers - @penta_numbers
+        elsif @sqr_numbers.include?(fourth_number)
+          remaining_numbers = remaining_numbers - @sqr_numbers
+        elsif @tri_numbers.include?(fourth_number)
+          remaining_numbers = remaining_numbers - @tri_numbers
+        end
+
+        fourth_level_numbers = remaining_numbers
+        fourth_list = next_elements(fourth_number, remaining_numbers)
+
+        # unless fourth_list.empty?
+        #   puts "#{octal} => #{second_number} => #{third_number} => #{fourth_number} => #{fourth_list.join(', ')}\n\n"
+        # end
+        
+        fourth_list.each do |fifth_number|
+          remaining_numbers = fourth_level_numbers
+          if @hepta_numbers.include?(fifth_number)
+            remaining_numbers = remaining_numbers - @hepta_numbers
+          elsif @hexa_numbers.include?(fifth_number)
+            remaining_numbers = remaining_numbers - @hexa_numbers
+          elsif @penta_numbers.include?(fifth_number)
+            remaining_numbers = remaining_numbers - @penta_numbers
+          elsif @sqr_numbers.include?(fifth_number)
+            remaining_numbers = remaining_numbers - @sqr_numbers
+          elsif @tri_numbers.include?(fifth_number)
+            remaining_numbers = remaining_numbers - @tri_numbers
+          end
+
+          fifth_level_numbers = remaining_numbers
+          fifth_list = next_elements(fifth_number, remaining_numbers)
+
+          # unless fifth_list.empty?
+          #   puts "#{octal} => #{second_number} => #{third_number} => #{fourth_number} => #{fifth_number} => #{fifth_list.join(', ')}\n\n"
+          # end
+          
+          fifth_list.each do |sixth_number|
+            last_list = next_elements(sixth_number, @octa_numbers)
+            last_list.each do |last_number|
+              if last_number == octal
+                puts "#{octal} => #{second_number} => #{third_number} => #{fourth_number} => #{fifth_number} => #{sixth_number} are cyclical!!"
+                puts "The sum is #{octal + second_number + third_number + fourth_number + fifth_number + sixth_number}\n\n"
+              end
+            end
+          end
+        end
+      end
+    end
   end
 end
 
-puts "\n\nTime spent is #{(Time.now - start_time) / 60 }  minutes"
+puts "\n\nTime spent is #{(Time.now - start_time) / 60 } minutes"
